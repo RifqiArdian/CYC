a695148618e6cb28463658205241eed361c6fdae
Rifqi Ardian <rifardian@gmail.com>
2018-02-01 09:33:47 +0700

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,8 +47,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:     'cycteamtea@gmail.com',
-    password:      'cycteamtea123',
+    user_name:     '',
+    password:      '',
     domain:        'gmail.com',
     address:       'smtp.gmail.com',
     port:          '587',
